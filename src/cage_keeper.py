15925af9db946a39fd094e9d99994f2b6c6ce3b6
Kenton Prescott <kentonprescott@gmail.com>
2019-10-28 23:15:15 -0700

    added new files

---
+++ src/cage_keeper.py
@@ -0,0 +1,150 @@
+# This file is part of the Maker Keeper Framework.
+#
+# Copyright (C) _______
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import logging
+import sys
+import time
+
+from web3 import Web3, HTTPProvider
+
+
+from pymaker import Address
+from pymaker.gas import DefaultGasPrice, FixedGasPrice
+from pymaker.keys import register_keys
+from pymaker.lifecycle import Lifecycle
+from pymaker.numeric import Wad
+from pymaker.token import ERC20Token
+
+class CageKeeper:
+    """Keeper to facilitate Emergency Shutdown"""
+
+    logger = logging.getLogger('simple-arbitrage-keeper')
+
+    def __init__(self, args, **kwargs):
+        """Pass in arguements assign necessary variables/objects and instantiate other Classes"""
+
+        parser = argparse.ArgumentParser("simple-arbitrage-keeper")
+
+        parser.add_argument("--rpc-host", type=str, default="localhost",
+                            help="JSON-RPC host (default: `localhost')")
+
+        parser.add_argument("--rpc-port", type=int, default=8545,
+                            help="JSON-RPC port (default: `8545')")
+
+        parser.add_argument("--rpc-timeout", type=int, default=10,
+                            help="JSON-RPC timeout (in seconds, default: 10)")
+
+        parser.add_argument("--eth-from", type=str, required=True,
+                            help="Ethereum address from which to send transactions; checksummed (e.g. '0x12AebC')")
+
+        parser.add_argument("--eth-key", type=str, nargs='*', required=True,
+                            help="Ethereum private key(s) to use (e.g. 'key_file=/path/to/keystore.json,pass_file=/path/to/passphrase.txt')")
+
+        parser.add_argument("--dss_deployment_file", type=str, required=True,
+                            help="Json description of all the system addresses (e.g. /Full/Path/To/configFile.json)")
+
+        parser.add_argument("--max-errors", type=int, default=100,
+                            help="Maximum number of allowed errors before the keeper terminates (default: 100)")
+
+        parser.add_argument("--debug", dest='debug', action='store_true',
+                            help="Enable debug output")
+
+        self.arguments = parser.parse_args(args)
+
+        self.web3 = kwargs['web3'] if 'web3' in kwargs else Web3(HTTPProvider(endpoint_uri=f"https://{self.arguments.rpc_host}:{self.arguments.rpc_port}",
+                                                                              request_kwargs={"timeout": self.arguments.rpc_timeout}))
+        self.web3.eth.defaultAccount = self.arguments.eth_from
+        register_keys(self.web3, self.arguments.eth_key)
+        self.our_address = Address(self.arguments.eth_from)
+
+        mcd = DssDeployment.from_json(web3=web3, conf=open(self.arguments.dss_deployment_file, "r").read())
+
+        self.max_errors = self.arguments.max_errors
+        self.errors = 0
+
+
+        logging.basicConfig(format='%(asctime)-15s %(levelname)-8s %(message)s',
+                            level=(logging.DEBUG if self.arguments.debug else logging.INFO))
+
+
+    def main(self):
+        """ Initialize the lifecycle and enter into the Keeper Lifecycle controller
+
+        Each function supplied by the lifecycle will accept a callback function that will be executed.
+        The lifecycle.on_block() function will enter into an infinite loop, but will gracefully shutdown
+        if it recieves a SIGINT/SIGTERM signal.
+
+        """
+
+        with Lifecycle(self.web3) as lifecycle:
+            self.lifecycle = lifecycle
+            lifecycle.on_block(self.process_block)
+
+
+
+
+
+    def process_block(self):
+        """Callback called on each new block. If too many errors, terminate the keeper to minimize potential damage."""
+        if self.errors >= self.max_errors:
+            self.lifecycle.terminate()
+        else:
+            self.check_cage()
+
+
+
+
+    def check_cage(self):
+        self.logger.info('Checking Cage')
+
+        #if cage has been called in End.sol:
+            #facilitate_cage()
+
+
+
+
+    def facilitate_cage(self):
+        self.logger.info('Facilitating Cage')
+
+        # Ilks = get_ilks( )
+        # drip_ilks(Ilks)
+        # flopIds = get_flopIds( )
+        # flapIds = get_flapIds( )
+        # flipIds = get_flipIds(ilk)
+        # yank_auctions(flopIds, flapIds)
+        # cage_ilks(ilks)
+        # skip_flip_auctions(Ilks, flipIds)
+        # Underwater_urns = get_underwater_urns(ilks)
+        # skim_urns(ilks, underwater_urns)
+
+
+
+
+
+
+
+    def gas_price(self):
+        """ FixedGasPrice if gas_price argument present, otherwise node DefaultGasPrice """
+        if self.arguments.gas_price > 0:
+            return FixedGasPrice(self.arguments.gas_price)
+        else:
+            return DefaultGasPrice()
+
+
+if __name__ == '__main__':
+    CageKeeper(sys.argv[1:]).main()
